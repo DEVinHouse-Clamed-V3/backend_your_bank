37138d5473a6fab13a972fe4b6278e96f4ac4a7b
Henrique Douglas <henriquedouglas2803@gmail.com>
2025-04-15 21:46:57 -0300

    adiciona documentação com swagger

--- src/controllers/RechargeController.ts
+++ src/controllers/RechargeController.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { Movement } from "../entities/Movement";
 import { AppDataSource } from "../data-source";
 import AppError from "../utils/AppError";
@@ -6,14 +6,70 @@ import { Client } from "../entities/Client";
 
 class RechargeController {
   private movementRepository;
-  private clientRepository;
 
   constructor() {
     this.movementRepository = AppDataSource.getRepository(Movement);
-    this.clientRepository = AppDataSource.getRepository(Client);
   }
 
-  create = async (req: Request, res: Response) => {
+  create = async (req: Request, res: Response, next: NextFunction) => {
+    /* 
+      #swagger.security = [{ "bearerAuth": [] }]
+
+      #swagger.path = '/recharge'
+      #swagger.tags = ['Recharge']
+      #swagger.summary = 'Realiza uma recarga de celular'
+      #swagger.description = 'Efetua uma recarga de celular para um número informado, debitando o valor do saldo do cliente.'
+
+      #swagger.requestBody = {
+        required: true,
+        content: {
+          "application/json": {
+            schema: {
+              type: "object",
+              properties: {
+                number: { type: "string", example: "11999999999" },
+                value: { type: "number", example: 50 },
+                operator: { type: "string", example: "Vivo" }
+              },
+              required: ["number", "value", "operator"]
+            }
+          }
+        }
+      }
+
+      #swagger.responses[201] = {
+        description: "Recarga realizada com sucesso",
+        content: {
+          "application/json": {
+            schema: {
+        $ref: "#/components/schemas/responseCreateRecharge"
+            }
+          }
+        }
+      }
+
+      #swagger.responses[400] = {
+        description: "Campos obrigatórios não informados",
+        content: {
+          "application/json": {
+            schema: {
+              $ref: "#/components/schemas/errorResponse"
+            }
+          }
+        }
+      }
+
+      #swagger.responses[500] = {
+        description: "Erro interno do servidor",
+        content: {
+          "application/json": {
+            schema: {
+                $ref: "#/components/schemas/errorResponse"
+            }
+          }
+        }
+      }
+  */
     try {
       const body = req.body;
 
@@ -21,12 +77,9 @@ class RechargeController {
         throw new AppError("Campos obrigatórios não informados", 400);
       }
 
-      const client = await this.clientRepository.findOneBy({
-        user_id: req?.userId,
-      });
 
       const lastMovement = await this.movementRepository.findOne({
-        where: { client_id: client?.id },
+        where: { client_id: req?.clientId },
         order: { created_at: "DESC" },
       });
 
@@ -38,12 +91,14 @@ class RechargeController {
         value: body.value,
         type: "SAIDA",
         description: `Recarga de ${body.value} para o número ${body.number}`,
-        client_id: client?.id,
+        client_id:  req?.clientId,
         balance: balance,
       });
 
       res.status(201).json(movement);
-    } catch {}
+    } catch(error) {
+      next(error)
+    }
   };
 }
 
